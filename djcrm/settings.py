c70293789907961ec7843632822c5ee8a921a6ed
Tom Byers <tmbyers1102@gmail.com>
2021-04-16 11:59:15 -0700

    added improved tailwind config and first tailwind build before the push. Should do tailwind build before every push and make sure those dependancies in tailwind.config.js are not popping up bc they cause the css not to read on site. will add comment around them

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     # Third party apps
     'crispy_forms',
     "crispy_tailwind",
+    'tailwind',
+    'theme',
 
     # Local apps
     'leads',
@@ -152,13 +154,14 @@ if not DEBUG:
 
     ALLOWED_HOSTS = ["*"]
 
-    # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    # EMAIL_HOST = env("EMAIL_HOST")
-    # EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-    # EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-    # EMAIL_USE_TLS = True
-    # EMAIL_PORT = env("EMAIL_PORT")
-    # DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = env("EMAIL_PORT")
+    DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+
 
 LOGGING = {
     'version': 1,
@@ -173,3 +176,5 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
+
+TAILWIND_APP_NAME = 'theme'
